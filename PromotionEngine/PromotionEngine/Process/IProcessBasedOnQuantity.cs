642b79139e310d35e5435dc3974b3bf855d1778d
Santosh <sansantosh.bg@gmail.com>
2020-06-05 18:21:04 +0530

    Added a interace IProcessBasedOnQuantity to secouple the promotion engine, in future we can apply other promotions too

---
+++ PromotionEngine/PromotionEngine/Process/IProcessBasedOnQuantity.cs
@@ -0,0 +1,12 @@
+﻿using PromotionEngine.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Process
+{
+    interface IProcessBasedOnQuantity
+    {
+        int ProcessAmoutBasedOnCountPromotion(Cart cart);
+    }
+}
---
+++ PromotionEngine/PromotionEngine/Process/ProcessBasedOnQuantity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PromotionEngine.Entities;
+
+namespace PromotionEngine.Process
+{
+    class ProcessBasedOnQuantity : IProcessBasedOnQuantity
+    {
+        public int ProcessAmoutBasedOnCountPromotion(Cart cart)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
