5e6fef7af51bfe165044cc4b26ca763d65c999c8
Santosh <sansantosh.bg@gmail.com>
2020-06-05 15:37:45 +0530

    Added the Promotion Rules interfaces

---
+++ PromotionEngine/PromotionEngine/PromotionRules/ISkuBCountRule.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.PromotionRules
+{
+    interface ISkuBCountRule
+    {
+        int GetPromotionAppliedAmountForSkuB(Cartitem cartitem);
+    }
+}
---
+++ PromotionEngine/PromotionEngine/PromotionRules/ISkuCandDCountRule.cs
@@ -0,0 +1,12 @@
+﻿using PromotionEngine.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.PromotionRules
+{
+    interface ISkuCandDCountRule
+    {
+        int GetPromotionAppliedAmountForSkuB(Cartitem cartitem);
+    }
+}
