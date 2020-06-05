6b9e3e39d52afe780d2bfb6df80a1f0d48e5551f
Santosh <sansantosh.bg@gmail.com>
2020-06-05 17:52:37 +0530

    Implemented Promotion rule factory

--- PromotionEngine/PromotionEngine/PromotionRules/PromotionRuleFactory.cs
+++ PromotionEngine/PromotionEngine/PromotionRules/PromotionRuleFactory.cs
@@ -8,7 +8,18 @@ namespace PromotionEngine.PromotionRules
     {
         public KsuCountRuleBase GetPromotionRule(string skuname)
         {
-            throw new NotImplementedException();
+            switch(skuname)
+            {
+                case "A":
+                    return new SkuACountRule();
+                case "B":
+                    return new SkuBCountRule();
+                case "C":
+                case "D":
+                    return new SkuCandDCountRule();
+                default:
+                    throw new Exception();
+            }
         }
     }
 }
