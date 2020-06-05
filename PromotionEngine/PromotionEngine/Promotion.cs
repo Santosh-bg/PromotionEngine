57f1f3b299ab661408171c7f4da4dd5d477d0116
Santosh <sansantosh.bg@gmail.com>
2020-06-05 18:08:36 +0530

    instantiate the classes using the factory pattern

--- PromotionEngine/PromotionEngine/Promotion.cs
+++ PromotionEngine/PromotionEngine/Promotion.cs
@@ -15,7 +15,7 @@ namespace PromotionEngine
             int amountForB = 0;
             int amountForCandD = 0;
             PromotionRuleFactory promotionRuleFactory = new PromotionRuleFactory();
-           // SkuACountRule skuAcountRule = new SkuACountRule();
+           
             var skuACartItems = cart.Cartitems.FindAll(x => x.Skuname.Equals("A"));
             if (skuACartItems != null && skuACartItems.Count > 0)
             {
@@ -23,15 +23,24 @@ namespace PromotionEngine
                 var obj= skuAcountRule as SkuACountRule;   
                 amountForA = obj.GetPromotionAppliedAmountForSkuA(skuACartItems[0]);
             }
-            SkuBCountRule skuBcountRule = new SkuBCountRule();
+         //   SkuBCountRule skuBcountRule = new SkuBCountRule();
             var skuBCartItems = cart.Cartitems.FindAll(x => x.Skuname.Equals("B"));
-            if(skuBCartItems!=null && skuBCartItems.Count>0)
-                amountForB = skuBcountRule.GetPromotionAppliedAmountForSkuB(skuBCartItems[0]);
+            if (skuBCartItems != null && skuBCartItems.Count > 0)
+            {
+                var skuAcountRule = promotionRuleFactory.GetPromotionRule(skuBCartItems[0].Skuname);
+                var obj = skuAcountRule as SkuBCountRule;
 
+                amountForB = obj.GetPromotionAppliedAmountForSkuB(skuBCartItems[0]);
+            }
             SkuCandDCountRule skuCandDcountRule = new SkuCandDCountRule();
             var skuCandDCartItems = cart.Cartitems.FindAll(x => x.Skuname.Equals("C") || x.Skuname.Equals("D"));
             if(skuCandDCartItems != null && skuCandDCartItems.Count>0)
+            {
+                var skuAcountRule = promotionRuleFactory.GetPromotionRule(skuCandDCartItems[0].Skuname);
+                var obj = skuAcountRule as SkuCandDCountRule;
                 amountForCandD = skuCandDcountRule.GetPromotionAppliedAmountForSkuCandD(skuCandDCartItems);
+            }
+                
 
             return TotalAmountAfterPromotionApplied = amountForA + amountForB + amountForCandD;
         }
